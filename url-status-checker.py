4e41942f9be246152485af402ce91ac3fe5d9b13
dusanmitrovic98 <dusanmitrovic@elfak.rs>
2023-07-22 16:44:00 +0200

    Update url-status-checker.py

--- url-status-checker.py
+++ url-status-checker.py
@@ -10,3 +10,4 @@ template = """
     <title>URL Status Checker</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/github-fork-ribbon-css/0.2.3/gh-fork-ribbon.min.css" />
     <style>
+        body {
