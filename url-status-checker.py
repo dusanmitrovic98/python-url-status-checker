7c8ddfc5dc7d1829c14cb0adb5276a3372f43761
dusanmitrovic98 <dusanmitrovic@elfak.rs>
2023-07-22 16:44:15 +0200

    Update url-status-checker.py

--- url-status-checker.py
+++ url-status-checker.py
@@ -11,3 +11,4 @@ template = """
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/github-fork-ribbon-css/0.2.3/gh-fork-ribbon.min.css" />
     <style>
         body {
+            font-family: Arial, sans-serif;
