a870b1505fbcc1b4a597d7848bbec85d67eb3283
dusanmitrovic98 <dusanmitrovic@elfak.rs>
2023-07-22 17:04:49 +0200

    Update url-status-checker.py

--- url-status-checker.py
+++ url-status-checker.py
@@ -79,3 +79,4 @@ def url_status_checker():
                 result = {"status": "error", "message": f"Failed to access URL. Status code: {response.status_code}"}
         except requests.exceptions.RequestException as e:
             result = {"status": "error", "message": f"Error occurred: {e}"}
+
